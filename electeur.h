0bf8db70ac893c7ed9cd9958a838ce3608f750e7
slimessai <116353775+slimessai@users.noreply.github.com>
2022-11-11 10:49:53 +0100

    Update electeur.h

--- electeur.h
+++ electeur.h
@@ -1,15 +1,26 @@
 #ifndef POINT_H_INCLUDED
 #define POINT_H_INCLUDED
 #include <stdio.h>
-typedef struct
+struct date
 {
-    char couleur [20];
-    int id, x,y;
-} point;
+    int day;
+    char month[30];
+    int year;
+};
+typedef struct date date;
+struct election
+{
+    int id ;
+    int nbre_habitant ;
+    char municipalite[50];
+    date date ;
+    int nbr_conseillers;
+};
+typedef struct election election;
 //le type de  retour int c'est pour indiquer si la tache a été réalisée avec succés ou non// pour afficher des lsg plus tard exemple erreur d'ouverture de fichier, element introuvable
-int ajouter(char *, point );
-int modifier( char *, int, point );
+int ajouter(char *, election );
+int modifier( char *, int, election );
 int supprimer(char *, int );
-point chercher(char *, int);
+election chercher(char *, int);
 
-#endif // electeur_H_INCLUDED
+#endif // POINT_H_INCLUDED
